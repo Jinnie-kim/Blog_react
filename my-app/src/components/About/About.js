5abb0a6185a6fb3ecc8fde384b0ea4debef760ca
Jinnie-kim <hjk7639@gmail.com>
2022-06-23 21:23:29 +0900

    feat: About UI 구현
    About UI를 구현했습니다.

---
+++ my-app/src/components/About/About.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import profile from '../../assets/profile.jpg';
+import facebook from '../../assets/Facebook.svg';
+import twitter from '../../assets/Twitter.svg';
+import instagram from '../../assets/Instagram.svg';
+import github from '../../assets/Github.svg';
+
+import './About.css';
+
+const About = () => {
+    return (
+        <aside className='about'>
+            <h2>About Me</h2>
+            <img src={profile} alt="" className='user-profile'/>
+            <p className="user-name">Chilli</p>
+			<p className="user-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+			<h3>Categories</h3>
+            <ul className="categories">
+				<li><a href="#">Life</a></li>
+				<li><a href="#">Style</a></li>
+				<li><a href="#">Tech</a></li>
+				<li><a href="#">Sport</a></li>
+				<li><a href="#">Photo</a></li>
+				<li><a href="#">Develop</a></li>
+				<li><a href="#">Music</a></li>
+			</ul>
+            <h3>Follow</h3>
+            <ul className="sns">
+				<li>
+					<a href="#">
+						<img src={facebook} alt="Facebook" />
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src={twitter} alt="Twitter" />
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src={instagram} alt="Instagram" />
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src={github} alt="GitHub" />
+					</a>
+				</li>
+			</ul>
+        </aside>
+    )
+}
+
+export default About;
