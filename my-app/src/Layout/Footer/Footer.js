15b0169692d779554750dfece16ac6c6f9771937
Jinnie-kim <hjk7639@gmail.com>
2022-06-23 21:46:06 +0900

    feat: Footer UI 구현
    Footer UI를 구현했습니다.

---
+++ my-app/src/Layout/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='max-width'>
+                <h2>©Weniv Corp.</h2>
+                <button className="top-button">TOP</button>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
