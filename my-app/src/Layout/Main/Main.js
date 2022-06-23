8c0a6b20e3ee5ba70489be25043af5e3d5c7180a
Jinnie-kim <hjk7639@gmail.com>
2022-06-23 15:35:01 +0900

    feat: Main UI 구현
    Main UI를 구현했습니다.

---
+++ my-app/src/Layout/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main>
+            <div className="max-width">
+                <h2 className='a11y-hidden'>Post</h2>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
