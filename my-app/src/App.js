7a5cdfe00e16a6175843ab454c97fc8db0d6d688
Jinnie-kim <hjk7639@gmail.com>
2022-06-23 21:50:58 +0900

    feat: App.js 파일에 Footer 컴포넌트 추가
    App.js 파일에 Footer 컴포넌트를 추가했습니다.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import Header from './Layout/Header/Header';
 import Banner from './Layout/Banner/Banner';
 import Main from './Layout/Main/Main';
+import Footer from './Layout/Footer/Footer';
 
 import './Reset.css';
 import './Global.css';
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Banner />
       <Main />
+      <Footer />
     </div>
   );
 }
