55f81c4356bd8fc2b3f863188fdca58e1a09531c
Jinnie-kim <hjk7639@gmail.com>
2022-06-23 20:40:21 +0900

    feat: Main 파일에 Posts 컴포넌트 추가
    Main 파일에 Posts 컴포넌트를 추가했습니다.

--- my-app/src/Layout/Main/Main.js
+++ my-app/src/Layout/Main/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Posts from '../../components/Posts/Posts';
+
 import './Main.css';
 
 const Main = () => {
@@ -7,6 +9,7 @@ const Main = () => {
         <main>
             <div className="max-width">
                 <h2 className='a11y-hidden'>Post</h2>
+                <Posts />
             </div>
         </main>
     )
